3d60907576b826fd231f9c9355a1f8e4cf2d266b
RafaelEmery <rafael.emerycade@gmail.com>
2025-09-18 19:57:45 -0300

    tests: adding test to validate failed transaction

--- playground/payments/tests/test_service.py
+++ playground/payments/tests/test_service.py
@@ -1,9 +1,12 @@
 from decimal import Decimal
+from unittest.mock import patch
 
 import pytest
 
 from payments.enums import ExpectedFees, PayableStatus, TransactionStatus
-from payments.models import Balance, Customer, Transaction, TransactionMethod
+from payments.exceptions import TransactionFailedError
+from payments.factory import CreditCardTransaction, DebitCardTransaction
+from payments.models import Balance, Transaction, TransactionMethod
 from payments.services import TransactionService
 
 
@@ -27,7 +30,7 @@ from payments.services import TransactionService
         ),
     ]
 )
-def test_process_credit_transaction_success(
+def test_process_transaction_success(
     customer_with_balance,
     method,
     expected_fee,
@@ -47,10 +50,8 @@ def test_process_credit_transaction_success(
         "card_verification_code": "123",
     })
 
-    customer_id = result["customer_id"]
-    customer = Customer.objects.get(id=customer_id)
-    balance = Balance.objects.get(customer=customer)
-    payables = customer.payable_set.all()
+    balance = Balance.objects.get(customer=customer_with_balance)
+    payables = customer_with_balance.payable_set.all()
     transactions = Transaction.objects.filter(
         id__in=payables.values_list("transaction_id", flat=True)
     ).distinct()
@@ -58,7 +59,7 @@ def test_process_credit_transaction_success(
     payable = payables.first()
     transaction = transactions.first()
 
-    assert customer is not None
+    assert str(result["customer_id"]) == str(customer_with_balance.id)
     assert payable is not None
     assert balance is not None
     assert balance_history is not None
@@ -70,3 +71,40 @@ def test_process_credit_transaction_success(
     assert float(balance.available) == pytest.approx(expected_available)
     assert float(balance_history.waiting_funds) == pytest.approx(0.00)
     assert float(balance_history.available) == pytest.approx(1000.00)
+
+
+@pytest.mark.django_db
+@pytest.mark.parametrize(
+    ("method", "factory_class"),
+    [
+        (TransactionMethod.CREDIT, CreditCardTransaction),
+        (TransactionMethod.DEBIT, DebitCardTransaction)
+    ]
+)
+def test_process_transaction_failure(customer_with_balance, method, factory_class):
+    service = TransactionService()
+    with patch.object(
+        factory_class,
+        "apply_payable_on_balance",
+        side_effect=TransactionFailedError("Arerê! O Vasco vai jogar a série B!")
+    ):
+        with pytest.raises(TransactionFailedError):
+            service.process({
+                "customer_id": customer_with_balance.id,
+                "value": 10.0,
+                "description": "Eu, sempre te amarei! Onde estiver, estarei! Ó meu Mengo!",
+                "method": method,
+                "card_number": "Júlio César",
+                "card_owner": "Petković",
+                "card_expiration_year": "2028",
+                "card_verification_code": "123",
+            })
+
+        payables = customer_with_balance.payable_set.all()
+        transactions = Transaction.objects.filter(
+            id__in=payables.values_list("transaction_id", flat=True)
+        ).distinct()
+        transaction = transactions.first()
+
+        assert transaction is not None
+        assert transaction.status == TransactionStatus.FAILED
